8b7abc04ac1a33b5ce5d0dc965d372a1269c7eec
mxve <68632137+mxve@users.noreply.github.com>
2025-07-03 17:43:01 +0200

    update socials

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,28 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-output: 'static',
-site: 'https://docs.iw4x.dev',
+	output: 'static',
+	site: 'https://docs.iw4x.dev',
 	integrations: [
 		starlight({
 			title: 'IW4x Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/iw4x' }],
+			social: [
+				{ 
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/iw4x'
+				},
+				{
+					icon: 'blueSky',
+					label: 'Bluesky',
+					href: 'https://bsky.app/profile/iw4x.dev',
+				},
+				{
+					icon: 'discord',
+					label: 'Discord',
+					href: 'https://discord.com/invite/pV2qJscTXf',
+				},
+			],
 			sidebar: [
 				// {
 				// 	label: 'Guides',
@@ -25,9 +41,9 @@ site: 'https://docs.iw4x.dev',
 		}),
 	],
 
-  vite: {
-    server: {
-      allowedHosts: ['docs.iw4x.dev']
-    }
-  }
+	vite: {
+		server: {
+			allowedHosts: ['docs.iw4x.dev']
+		}
+	}
 });
